53205f22e78ab1b8a51c4102a9bb89a0a23a5d16
wildanrhmn <80866806+wildanrhmn@users.noreply.github.com>
2024-07-08 00:32:30 +0700

    feat: add jwtService export

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
+//Guards
 export * from './guards/jwt.guard';
 export * from './guards/google-oauth.guard';
 export * from './guards/facebook-oauth.guard';
-
+//Strategies
 export * from './strategies/jwt-auth.strategy';
 export * from './strategies/google-oauth.strategy';
 export * from './strategies/facebook-oauth.strategy';
-
+//Modules
 export * from './modules/auth.module';
+//Services
+export { JwtService } from '@nestjs/jwt';
